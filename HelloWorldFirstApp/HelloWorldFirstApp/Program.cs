8cfec342da8933cf12b7dbd77f8f8d72d123dc42
Daniel Sutton <danielterencesutton@gmail.com>
2016-11-21 17:49:42 +0000

    Signed off by Dan

---
+++ HelloWorldFirstApp/HelloWorldFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quick app showcasing Hello World for Github
+            Console.WriteLine("Hello C# World");
+        }
+    }
+}
